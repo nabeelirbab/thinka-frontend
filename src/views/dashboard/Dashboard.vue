d7fb017b280b9713d333b30fd0d231f48737f310
Jamesh <jamesh@autozone.com.au>
2020-12-15 23:11:21 +1100

    Dashboard: Display welcome box. Change arrangement of cards.

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="container py-4 bg-white height-max">
     <template v-if="user">
-      <h2>Dashboard</h2>
-      <p>Good day! In your dashboard, you will see different information and functionality to help and guide you in using <strong class="text-uppercase">Thinka.io</strong>.</p>
+      <div id="welcomebox" class="container pb-4" v-if="welcomeVisible">
+        <div class="d-flex border p-2 border rounded-lg">
+        <p>Good day! In your dashboard, you will see information and functionality to help guide you in using <strong class="text-uppercase">Thinka.io</strong>.<router-link to="/learning"> Click here to visit the learning centre. <fa icon="school" /></router-link><br></p>
+        <div><fa v-on:click="welcomeVisible = false" icon="window-close" /></div>
+      </div>
+      </div>
     </template>
     <div class='text-center mb-2'>
       <router-link to="/new-statement" class="btn btn-lg btn-primary text-uppercase mx-1 mb-2"><fa icon="project-diagram" /> Create New Tree</router-link>
@@ -10,12 +14,9 @@
     </div>
     <div class="row no-gutters">
       <div class="col-12 col-md-8 pr-md-2 pb-3">
-        <Trending />
-      </div>
-      <div class="col-12 col-md-4 pb-3">
         <MyList v-if="user" class="mb-2" />
         <div class="card mb-2">
-          <h5 class="card-header"><fa icon="cloud" /> Thinka Tank</h5>
+          <h5 class="card-header"><fa icon="cloud" /> My Thinka Tanks</h5>
           <div class="card-body">
             <p class="card-text">Thinka Tanks can contain many logic trees and many users so they can chat and assist collaboration privately.</p>
             <table class="table ">
@@ -32,7 +33,9 @@
             </table>
           </div>
         </div>
-        
+      </div>
+      <div class="col-12 col-md-4 pb-3">
+        <Trending />
       </div>
     </div>
   </div>
@@ -51,7 +54,8 @@ export default {
   },
   data(){
     return {
-      user: Auth.user()
+      user: Auth.user(),
+      welcomeVisible: true
     }
   }
 }
