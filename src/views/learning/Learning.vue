bdcbad45dea61f58c4209f073176da8c2eae862f
Jamesh <jamesh@autozone.com.au>
2020-12-16 10:16:56 +1100

    Update text on learning page.

--- src/views/learning/Learning.vue
+++ src/views/learning/Learning.vue
@@ -5,9 +5,13 @@
       </div>
       <div class="col-12 col-md-6 p-4 order-first order-md-last" >
         <div class="text-content mt-md-4 pt-md-4 p-3 text-left" >
-          <h2 class="mb-4 text-primary text-left">Welcome to <span class="">THINKA.IO!</span></h2>
+          <h3 class="mb-4 text-primary text-left">Welcome to <span class="">THINKA.IO!</span></h3>
           <p class="lead text-justify" >Thinka.io helps users organize their thoughts into logical trees. Each sentence is backed up by supportive points or rebutted by counter points.</p>
-          <p class="lead text-justify" >Users can clearly express why they think the way they do. People can create and share <em>Points of View</em> and see how many people share the same point of view</p>
+          <p class="lead text-justify" >By creating trees users can clearly express why they think the way they do. People can create and share <em>points of view</em> and see how many people share the same view.</p>
+          <p class="lead text-justify">Symbols are used to connect sentences so that readers know the logical basis of each point of view.</p>
+          <div class="text-danger font-weight-bold mr-1" style="font-size:1.5em">+ Support     - Counter     * Note</div>
+          <p class="lead text-justify"><br>When a sentence is marked with a symbol that indicates the sentence has impact so that agreements will cascade according to the lines of reasoning defined in the branches of the logical tree.<br><router-link to="/login" class="text-nowrap">Learn more about symbols.</router-link></p>
+`
           <router-link to="/register" class="btn btn-primary btn-lg text-nowrap mb-2">Join Community Now</router-link>
           <br />
           Already have an account? <router-link to="/login" class="text-nowrap font-weight-bold">Log In Now</router-link>
