f6596fa588538f05373759018e19aa2f0d696fd1
Jamesh <jamesh@autozone.com.au>
2020-12-31 16:47:52 +1100

    MoreMenu: Add learning centre link.

--- src/views/more-menu/MoreMenu.vue
+++ src/views/more-menu/MoreMenu.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container pt-4">
-    <h5>Under Construction</h5>
+    <h5>Learning Centre</h5>
+    <router-link to="/learning"> Click here to visit the learning centre. <fa icon="school" /></router-link>
+    <br>
   </div>
 </template>
