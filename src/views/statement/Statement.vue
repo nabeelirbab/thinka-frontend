d57cb136386aa413dd0c4f25b9ad88bd5b9940e3
Jamesh <jamesh@autozone.com.au>
2021-04-13 10:03:37 +1000

    Statement: Change edit and create box to be full width without overflow.

--- src/views/statement/Statement.vue
+++ src/views/statement/Statement.vue
@@ -31,7 +31,7 @@
           <div @click="setSeparatorWindowSupports" class="text-center text-light" style="cursor: pointer;">
             - &nbsp;SUPPORTS&nbsp; -
           </div>
-          <div ref="positiveWindow" class="statement-window " :style="{height: positiveStatementHeight + 'px', 'max-height': (totaRelevanceWindowHeight - 50) + 'px', 'min-height': (20) + 'px'}">
+          <div ref="positiveWindow" class="statement-window" :style="{height: positiveStatementHeight + 'px', 'max-height': (totaRelevanceWindowHeight - 50) + 'px', 'min-height': (20) + 'px'}">
             <draggable
               @start="startDragging(true)"
               @end="endDragging"
@@ -486,7 +486,7 @@ export default {
   padding-bottom: 0px
 }
 .statement-window {
-  /* overflow:visible; */
+  overflow-x: hidden;
   overflow-y: auto;
   padding:0px 5px
 }
